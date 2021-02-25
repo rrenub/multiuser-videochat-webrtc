5a0584e9b322fd7ba3824a4718fac27a365d27ea
Ruben Delgado <cuenta.rubendelgado@gmail.com>
2021-02-25 00:12:46 +0000

    added path to / express

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+const path = require('path')
 const express = require('express')
 const app = express()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 
-app.use('/', express.static('public'))
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 io.on('connection', (socket) => {
   socket.on('join', (payload) => {
